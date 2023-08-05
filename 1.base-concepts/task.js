3ab4bd010a4cf9d3fa1f11e575d6a3f55b04461e
Dima <klinboo61@gmail.com>
2023-08-05 21:11:44 +0300

    homework1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,30 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let x1, x2;
+  const D = Math.pow(b, 2) - 4 * a * c;
+
+  if (D < 0) {
+    return [];
+
+  } else if (D === 0) {
+    x1 = -b  / (2 * a);
+    arr.push(x1);
+
+  } else if (D > 0) {
+    x1 = (-b + Math.sqrt(D)) / (2 * a);
+    x2 = (-b - Math.sqrt(D)) / (2 * a);
+    arr.push(x1);
+    arr.push(x2);
+  }
   return arr;
-}
+};
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let newPercent = percent / 100 / 12;
+  let totalAmount = amount - contribution;
+  let result = totalAmount * (newPercent + (newPercent / (((1 + newPercent) ** countMonths) - 1)));
+  return +(result * countMonths).toFixed(2);
 }
